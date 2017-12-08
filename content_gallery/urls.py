1bc9106e8cff8e1e325ad0a11540b082e9f49a36
Phoebe Bright <phoebebright310@gmail.com>
2017-12-08 18:51:36 +0000

    django2 only

--- content_gallery/urls.py
+++ content_gallery/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
@@ -6,15 +6,15 @@ app_name = 'content_gallery'
 
 urlpatterns = [
     # a URL for getting a list of objets of the model
-    url(
-        r'^ajax/choices/(?P<pk>\d+)/$',
+    path(
+        '^ajax/choices/<int:pk>/$',
         views.choices,
         name='choices'
     ),
     # a URL for getting data of all images related to the object
-    url(
-        r'^ajax/gallery_data/(?P<app_label>\w+)/'
-        '(?P<content_type>\w+)/(?P<object_id>\d+)/$',
+    path(
+        '^ajax/gallery_data/<str:app_label>/'
+        '<str:content_type>)/<int:object_id>)/$',
         views.gallery_data,
         name='gallery_data'
     ),
